c7001bf2c3431c138e99e1919cebbd19b63d8156
Darko Novak <novak.darko1@gmail.com>
2021-10-13 19:50:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+num = int(input("Select number between 1 and 100: "))
+
+for fizzbuzz in range(1, num+1):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
